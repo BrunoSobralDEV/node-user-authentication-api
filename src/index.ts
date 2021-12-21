564044a3aa5f017caeab0bde68972547845f04fa
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-12-21 15:00:49 -0300

    Typescript configurado e automatizando o build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Executei !')
+import express, { Request, Response, NextFunction} from "express";
+
+const app = express();
+
+//Configurar as rotas
+app.get('/status', (req: Request, res: Response, next:NextFunction) =>{
+    res.status(200).send({ foo: 'bar'})    
+})
+
+app.listen(3000, () =>{
+    console.log('Aplicação executando na http://localhost:3000')
+})
