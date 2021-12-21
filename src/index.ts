a9c468b8d18d3af9d0fb8bb38ddff927650c6cec
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-12-21 16:15:59 -0300

    express-json-urlencoded

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+// Configurações da aplicação - entender JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 //Configurar as rotas
 app.use(usersRoute);
 
@@ -10,6 +14,7 @@ app.get('/status', (req: Request, res: Response, next:NextFunction) =>{
     res.status(200).send({ foo: 'Sucesso !'})    
 })
 
+// Inicialização do servidor
 app.listen(3000, () =>{
-    console.log('Aplicação executando na http://localhost:3000')
+    console.log('Aplicação sendo executada na http://localhost:3000')
 })
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,6 +3,7 @@ import { Router, Response, Request, NextFunction } from "express";
 
 const usersRoute = Router();
 
+//Read
 usersRoute.get('/users', (req: Request, res: Response, next:NextFunction) =>{
     //lista de usuários
     const users = [{ userName: 'Bruno' }];
@@ -15,4 +16,10 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, ne
     res.status(StatusCodes.OK).send({ uuid });
 });
 
+//Create
+usersRoute.post('/users', (req: Request, res: Response, next:NextFunction) =>{
+    const newUser = req.body;
+    console.log(req.body)
+    res.status(StatusCodes.CREATED).send(newUser);
+})
 export default usersRoute;
